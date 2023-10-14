2d79575f60dc35ecba305cc71e82ee3d632c17cb
Pavlo Kozub <pavlo.kozub@gmail.com>
2023-10-14 19:20:01 +0300

    Initial commit

---
+++ test/diploma.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Diploma from '../lib/diploma-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/diploma-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Diploma.DiplomaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
